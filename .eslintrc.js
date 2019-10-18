154ce78a279facb8831967c2e97ad4f89fb0589a
Brett Zamir <brettz9@yahoo.com>
2019-10-18 22:06:45 +0800

    - Linting: Remove disabling of unused rule; sort/ws

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,44 @@
 /* eslint-env node */
 module.exports = {
-    env: {
-        browser: true,
-        es6: true
-    },
-    settings: {
-        polyfills: [
-          'JSON',
-          'Object.assign'
-        ]
-    },
-    overrides: [{
-        files: ['docs/jsdoc-config.js'],
-        rules: {
-          strict: 0,
-          'import/unambiguous': 0,
-          'import/no-commonjs': 0
-        }
-    }, {
-      files: ['**/*.md'],
-      rules: {
-        'eol-last': ['off'],
-        'no-console': ['off'],
-        'no-undef': ['off'],
-        'no-unused-vars': ['off'],
-        'padded-blocks': ['off'],
-        'import/unambiguous': ['off'],
-        'import/no-unresolved': ['off'],
-        'node/no-missing-import': ['off']
-      }
-    }],
-    extends: ['ash-nazg/sauron'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module'
-    },
+  extends: ['ash-nazg/sauron'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    es6: true
+  },
+  settings: {
+    polyfills: [
+      'JSON',
+      'Object.assign'
+    ]
+  },
+  overrides: [{
+    files: ['docs/jsdoc-config.js'],
     rules: {
-        'valid-jsdoc': 0
+      strict: 0,
+      'import/unambiguous': 0,
+      'import/no-commonjs': 0
     }
+  }, {
+    files: ['**/*.md'],
+    rules: {
+      'eol-last': ['off'],
+      'no-console': ['off'],
+      'no-undef': ['off'],
+      'no-unused-vars': ['off'],
+      'padded-blocks': ['off'],
+      'import/unambiguous': ['off'],
+      'import/no-unresolved': ['off'],
+      'node/no-missing-import': ['off']
+    }
+  }],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  rules: {
+  }
 };
